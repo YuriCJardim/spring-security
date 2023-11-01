8bb280b6bb7e5e90dd02a49edf127adbc37dcd59
YuriCJardim <yuricjardim@gmail.com>
2023-11-01 10:54:42 -0300

    feat - adicionando a classe controller, WelcomeController

--- src/main/java/com/correa/springsecurity/WelcomeController.java
+++ src/main/java/com/correa/springsecurity/WelcomeController.java
@@ -2,7 +2,9 @@ package com.correa.springsecurity;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class WelcomeController {
     @GetMapping
     public String welcome(){
