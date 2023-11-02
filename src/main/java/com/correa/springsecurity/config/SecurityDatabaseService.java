796f24bc5b30ba1d7405c55547bb74c6e4fd0edd
YuriCJardim <yuricjardim@gmail.com>
2023-11-02 11:28:54 -0300

    feat - refatorando para adicionar o banco de dados, e adicionando o banco de dados H2

---
+++ src/main/java/com/correa/springsecurity/config/SecurityDatabaseService.java
@@ -0,0 +1,45 @@
+package com.correa.springsecurity.config;
+
+import com.correa.springsecurity.model.User;
+import com.correa.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class SecurityDatabaseService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        userEntity.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        });
+        UserDetails user = new org.springframework.security.core.userdetails.User(userEntity.getUsername(),
+                userEntity.getPassword(),
+                authorities);
+        return user;
+    }
+
+
+}
--- src/main/java/com/correa/springsecurity/controller/WelcomeController.java
+++ src/main/java/com/correa/springsecurity/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.correa.springsecurity;
+package com.correa.springsecurity.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
